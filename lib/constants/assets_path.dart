d30ce28357a518eba358835bb4b1b34adcff4514
Medi G Fulgence <lycorisblue99@gmail.com>
2025-05-23 08:55:00 +0000

    Ajout de nouveaux chemins d'icônes et d'images pour l'application électorale, ainsi que la création du modèle NewsArticle avec des données d'exemple.

--- lib/constants/assets_path.dart
+++ lib/constants/assets_path.dart
@@ -4,8 +4,21 @@ class AppFont {
 
 class AppIcon {
   static const iconA = 'assets/icons/icon.svg';
+
+  // Icônes pour l'application électorale
+  static const iconHome = 'assets/icons/home_icon.svg';
+  static const iconCandidates = 'assets/icons/candidates_icon.svg';
+  static const iconAssistance = 'assets/icons/assistance_icon.svg';
+  static const iconMenu = 'assets/icons/menu_icon.svg';
 }
 
 class AppImage {
   static const image = "assets/images/image.png";
+
+  // Images pour l'application électorale
+  static const logo225Votes = "assets/images/logo_225_votes.png";
+  static const alassaneOuattaraBanner = "assets/images/alassane_ouattara_banner.jpg";
+  static const inscriptionElectorale = "assets/images/inscription_electorale.jpg";
+  static const debatPresidentiel = "assets/images/debat_presidentiel.jpg";
+  static const verificationVote = "assets/images/verification_vote.jpg";
 }
